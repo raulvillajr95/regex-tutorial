1a84686c99f6d35c9c20f7f0d2bac10be9d43283
Raul <raulvillajr95@gmail.com>
2022-04-15 20:08:38 -0700

    Add email validation in RegEx

--- scripts.js
+++ scripts.js
@@ -3,7 +3,10 @@ const inputs = document.querySelectorAll('input');
 
 const patterns = {
   telephone: /^\d{11}$/,
-  username: /^[a-z\d]{5,12}$/i
+  username: /^[a-z\d]{5,12}$/i,
+  password: /^[\w@-]{8,20}$/,
+  slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 };
 
 // validation function
